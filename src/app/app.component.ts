3ab2ee50d8ca9363fc45e75888511f5b834cdc34
waleed-cs <waleedalharthi1999@gmail.com>
2021-12-26 00:43:58 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'first-app';
+  array: string[] = [];
+  insert(txt: any) {
+    this.array.push(txt);
+  }
+  delete(i: any) {
+    i = this.array.indexOf(i);
+    console.log(i);
+    this.array.splice(i, 1);
+  }
 }
